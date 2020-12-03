c80f035b0c1b86e3d990e0e1da0a22eb7653e302
Alexandru Reznic <alexandru.reznic@energymarketprice.com>
2020-12-03 20:57:58 +0200

    add http dependency

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {environment} from 'src/environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-  protected baseUrl: string = environment.baseUrl;
-  constructor(private httpclient: HttpClient) { }
-  public register(argument: object): Observable<any> {
-    return this.httpclient.post<any>(this.baseUrl + 'api/auth/Register', argument);
-  }
+    protected baseUrl: string = environment.baseUrl;
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    public register(argument: object): Observable<any> {
+        return this.httpClient.post<any>(this.baseUrl + 'api/auth/Register', argument);
+    }
 }
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { AuthComponent } from './auth.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import {AuthRoutingModule} from './auth-routing.module';
+import {AuthComponent} from './auth.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
-  declarations: [AuthComponent, RegisterComponent, LoginComponent],
+    declarations: [AuthComponent, RegisterComponent, LoginComponent],
     imports: [
         CommonModule,
         AuthRoutingModule,
         ReactiveFormsModule
     ]
 })
-export class AuthModule { }
+export class AuthModule {
+}
