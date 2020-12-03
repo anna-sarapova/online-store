55984dc35f651d8b57c516ed1471a437d388b2f0
Alexandru Reznic <alexandru.reznic@energymarketprice.com>
2020-12-03 19:50:02 +0200

    add text voice on mouseover

---
+++ src/app/core/services/speech.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SpeechService {
+
+    constructor() {
+    }
+
+    private static textToAudio(text: string): void {
+        let speech = new SpeechSynthesisUtterance();
+        speech.lang = "en-US";
+        speech.text = text;
+        speech.volume = 1;
+        speech.rate = 1;
+        speech.pitch = 1;
+        window.speechSynthesis.speak(speech);
+    }
+
+    public textElements(event): void {
+        SpeechService.textToAudio(event.target.innerText);
+    }
+
+    public textInput(event): void {
+        SpeechService.textToAudio(event.target.placeholder);
+    }
+
+    public textImage(event): void {
+        SpeechService.textToAudio(event.target.alt);
+    }
+}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,28 +1,30 @@
 import {Component, OnInit} from '@angular/core';
 import {LinkModel} from "../../../core/models/link.model";
+import {SpeechService} from "../../../core/services/speech.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  public links: LinkModel[];
+export class HeaderComponent extends SpeechService implements OnInit {
+    public links: LinkModel[];
 
-  constructor() {
-  }
+    constructor() {
+        super();
+    }
 
-  ngOnInit(): void {
-    this.initLinks();
-  }
+    ngOnInit(): void {
+        this.initLinks();
+    }
 
-  private initLinks(): void {
-    this.links = [
-      {name: 'Home', route: '/home'},
-      {name: 'Categories'},
-      {name: 'Offers'},
-      {name: 'Contacts'}
-    ];
-  }
+    private initLinks(): void {
+        this.links = [
+            {name: 'Home', route: '/home'},
+            {name: 'Categories'},
+            {name: 'Offers'},
+            {name: 'Contacts'}
+        ];
+    }
 
 }
